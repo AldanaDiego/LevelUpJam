ba31f7409a780bca7b7f8057018b39f12eaeab6c
Diego Aldana <daldana9@gmail.com>
2023-07-18 12:26:20 -0400

    Game timer display

---
+++ Assets/Scripts/UI/GlobalTimerUI.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class GlobalTimerUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _startCountdownText;
+    [SerializeField] private TextMeshProUGUI _globalTimerText;
+
+    private GameTimer _gameTimer;
+    private bool _isGlobalTimerRunning;
+    private bool _hasTimerStarted;
+
+    private void Start()
+    {
+        _gameTimer = GameTimer.GetInstance();
+        _startCountdownText.text = "";
+        _isGlobalTimerRunning = false;
+        _hasTimerStarted = false;
+        UpdateGlobalTimeText();
+        _gameTimer.OnGlobalTimerStarted += OnGlobalTimerStarted;
+        _gameTimer.OnGlobalTimerEnded += OnGlobalTimerEnded;
+    }
+
+    private void Update()
+    {
+        if (_isGlobalTimerRunning)
+        {
+            UpdateGlobalTimeText();
+        }
+        else if (!_hasTimerStarted)
+        {
+            int startTime = Mathf.CeilToInt(_gameTimer.GetStartTimer());
+            if (startTime < 4)
+            {
+                _startCountdownText.text = startTime.ToString();
+            }
+        }
+    }
+
+    private void UpdateGlobalTimeText()
+    {
+        float timeLeft = _gameTimer.GetGlobalTimer();
+        int minutes = Mathf.FloorToInt(timeLeft / 60);
+        int seconds = Mathf.FloorToInt(timeLeft % 60);
+        _globalTimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    private void OnGlobalTimerStarted(object sender, EventArgs empty)
+    {
+        _isGlobalTimerRunning = true;
+        _hasTimerStarted = true;
+        _startCountdownText.text = "";
+    }
+
+    private void OnGlobalTimerEnded(object sender, EventArgs empty)
+    {
+        _isGlobalTimerRunning = false;
+    }
+}
