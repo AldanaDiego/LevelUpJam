45447ca0180cad817b90e272333230225daf0d10
Diego Aldana <daldana9@gmail.com>
2023-07-22 22:45:22 -0400

    Show score increase and decrease

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -5,7 +5,7 @@ using System;
 
 public class ScoreSystem : MonoBehaviour
 {
-    public event EventHandler<int> OnScoreChanged;
+    public event EventHandler<ScoreChangedParams> OnScoreChanged;
     private int _score;
 
     private void Start()
@@ -20,25 +20,26 @@ public class ScoreSystem : MonoBehaviour
     private void OnCustomerServed(object sender, EventArgs empty)
     {
         _score += 1;
-        OnScoreChanged?.Invoke(this, _score);
+        OnScoreChanged?.Invoke(this, new ScoreChangedParams(){Score = _score, ScoreDiff = 1});
     }
 
     private void OnCustomerSuccess(object sender, EventArgs empty)
     {
         _score += 5;
-        OnScoreChanged?.Invoke(this, _score);
+        OnScoreChanged?.Invoke(this, new ScoreChangedParams(){Score = _score, ScoreDiff = 5});
     }
 
     private void OnCustomerFail(object sender, EventArgs empty)
     {
+        int scoreDiff = (_score >= 3) ? -3 : _score * -1;
         _score = Math.Max(0, _score - 3);
-        OnScoreChanged?.Invoke(this, _score);
+        OnScoreChanged?.Invoke(this, new ScoreChangedParams(){Score = _score, ScoreDiff = scoreDiff});
     }
 
     private void OnGameRestart(object sender, EventArgs empty)
     {
         _score = 0;
-        OnScoreChanged?.Invoke(this, _score);
+        OnScoreChanged?.Invoke(this, new ScoreChangedParams(){Score = _score, ScoreDiff = 0});
     }
 
     private void OnDestroy()
@@ -47,5 +48,11 @@ public class ScoreSystem : MonoBehaviour
         Customer.OnCustomerSuccess -= OnCustomerSuccess;
         Customer.OnCustomerFail -= OnCustomerFail;
         GameEndMenuUI.OnGameRestart -= OnGameRestart;
+    }
+
+    public class ScoreChangedParams: EventArgs
+    {
+        public int Score;
+        public int ScoreDiff;
     }
 }
--- Assets/Scripts/UI/ScoreUI.cs
+++ Assets/Scripts/UI/ScoreUI.cs
@@ -7,14 +7,28 @@ public class ScoreUI : MonoBehaviour
 {
     [SerializeField] private ScoreSystem _scoreSystem;
     [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TextMeshProUGUI _scoreDiffText;
 
     private void Start()
     {
         _scoreSystem.OnScoreChanged += OnScoreChanged;
     }
 
-    private void OnScoreChanged(object sender, int newScore)
+    private IEnumerator ShowScoreDiff(int scoreDiff)
     {
-        _scoreText.text = $"{newScore}";
+        _scoreDiffText.color = (scoreDiff > 0) ? Color.green : Color.red;
+        string sign = (scoreDiff > 0) ? "+" : "";
+        _scoreDiffText.text = $"{sign}{scoreDiff}";
+        yield return new WaitForSeconds(0.5f);
+        _scoreDiffText.text = "";
+    }
+
+    private void OnScoreChanged(object sender, ScoreSystem.ScoreChangedParams scoreChange)
+    {
+        _scoreText.text = $"{scoreChange.Score}";
+        if (scoreChange.ScoreDiff != 0)
+        {
+            StartCoroutine(ShowScoreDiff(scoreChange.ScoreDiff));
+        }
     }
 }
